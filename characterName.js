a4ea0fe4d85d12f6eb6c6844a12784a86076cffb
henniyoon <2749126@naver.com>
2023-11-07 22:50:52 +0900

    새벽 길드원 목록 크롤링

--- characterName.js
+++ characterName.js
@@ -1,15 +1,17 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const mainCharacterName = [];
+
+const mainCharacterName = []; // 본캐 길드 닉네임 저장 배열
+const subCharacterName = [];  // 부캐 길드 닉네임 저장 배열
 const numPages = 10; // 크롤링할 페이지 수
 
-async function scrapeRankTable(url) {
+async function scrapeRankTable(url, array) {
   try {
     const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
 
-    // class가 rank_table인 표를 식별합니다.
+    // class가 rank_table인 표를 식별
     const rankTable = $("table.rank_table");
 
     // 표에서 a 태그 안의 정보를 추출
@@ -17,7 +19,7 @@ async function scrapeRankTable(url) {
       $(row)
         .find("td a")
         .each((colIndex, link) => {
-          mainCharacterName.push($(link).text().trim());
+          array.push($(link).text().trim());
         });
     });
   } catch (error) {
@@ -29,13 +31,16 @@ async function scrapeData() {
   const promises = [];
 
   for (let i = 1; i <= numPages; i++) {
-    const url = `https://maplestory.nexon.com/Common/Guild?gid=402812&wid=0&orderby=1&page=${i}`;
-    promises.push(scrapeRankTable(url));
+    const mainUrl = `https://maplestory.nexon.com/Common/Guild?gid=402812&wid=0&orderby=1&page=${i}`;
+    const subUrl = `https://maplestory.nexon.com/Common/Guild?gid=312443&wid=0&orderby=1&page=${i}`;
+    promises.push(scrapeRankTable(mainUrl, mainCharacterName));
+    promises.push(scrapeRankTable(subUrl, subCharacterName));
   }
 
   await Promise.all(promises);
 
   console.log(mainCharacterName);
+  console.log(subCharacterName);
 }
 
 scrapeData();
