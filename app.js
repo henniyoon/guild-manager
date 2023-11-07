85025c7b5baa6a378aa271fc521db3c4cab13829
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-11-07 23:34:32 +0900

    localhost 구축

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000; // 사용할 포트 번호
+
+// public 폴더 내의 정적 파일 (index.html)을 제공
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 서버를 시작
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
