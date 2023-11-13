345f1ca106345e416bc405c417b5624e1adeaede
henniyoon <2749126@naver.com>
2023-11-13 22:57:16 +0900

    포트 번호 3000 -> 3030 변경

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000; // 사용할 포트 번호
+const port = 3030; // 사용할 포트 번호
 
 // public 폴더 내의 정적 파일 (index.html)을 제공
 app.use(express.static(path.join(__dirname, 'public')));
