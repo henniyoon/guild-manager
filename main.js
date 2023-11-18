b35c6a3664705ef7e43ca0b50cbfde4b3a43ca71
henniyoon <2749126@naver.com>
2023-11-18 23:23:58 +0900

    main_member 테이블에서 orm 모듈로 name 불러오기

--- main.js
+++ main.js
@@ -20,7 +20,10 @@ async function fetchData() {
         await MainMember.sync();
 
         // 데이터 조회
-        const members = await MainMember.findAll();
+        const members = await MainMember.findAll({
+            attributes: ['name'],
+            raw: true,  // 반환 데이터를 Sequelize 모델 객체가 아닌 순수 JSON 객체로 변경
+        });
         console.log('조회 결과: ', members);
     } catch (error) {
         console.error('데이터베이스 조회 에러: ', error);
