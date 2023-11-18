3bd0473cf4c04964e77a6bd9e93a3daeed638bd3
henniyoon <2749126@naver.com>
2023-11-18 23:08:56 +0900

    orm 모듈을 이용한 데이터 조회

---
+++ main.js
@@ -0,0 +1,34 @@
+const { Sequelize } = require('sequelize');
+const MainMember = require('./models/mainMember.js');
+
+async function fetchData() {
+    const sequelize = new Sequelize({
+        dialect: 'mariadb',
+        host: 'localhost',
+        port: '3307',
+        username: 'root',
+        password: '0000',
+        database: 'guild_manager',
+    });
+
+    try {
+        // 데이터베이스 연결 확인
+        await sequelize.authenticate();
+        console.log('데이터베이스 연결 성공');
+        
+        // 데이터베이스에 테이블이 없다면 생성
+        await MainMember.sync();
+
+        // 데이터 조회
+        const members = await MainMember.findAll();
+        console.log('조회 결과: ', members);
+    } catch (error) {
+        console.error('데이터베이스 조회 에러: ', error);
+    } finally {
+        // sequelize.close();  // close 메서드는 더이상 필요하지 않습니다.
+        console.log('데이터베이스 연결 종료');
+    }
+}
+
+// 데이터 조회 함수 실행
+fetchData();
