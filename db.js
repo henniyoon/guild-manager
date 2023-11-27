c55e310b47768c8f32aac259b7d164f0fbdca45b
henniyoon <2749126@naver.com>
2023-11-28 02:35:38 +0900

    config 생성 파일 연결, 기능 분리

--- db.js
+++ db.js
@@ -1,34 +1,7 @@
 const { Sequelize } = require('sequelize');
+const config = require('./config/config.json');
 
-// MariaDB 연결 정보
-const sequelize = new Sequelize({
-    dialect: 'mariadb',
-    host: 'localhost',
-    port: '3307',
-    username: 'root',
-    password: '0000',
-    database: 'guild_manager',
-});
+const env = process.env.NODE_ENV || 'development';
+const sequelize = new Sequelize(config[env]);
 
-// 배열의 각 요소를 데이터베이스에 저장
-async function saveMember(characterNames) {
-    let conn;
-
-    try {
-        conn = await pool.getConnection();
-
-        for (const name of characterNames) {
-            await conn.query('INSERT INTO main_member (name) VALUES (?) ', [name]);
-        }
-
-        console.log('별빛 길드원 저장 성공');
-    } catch (err) {
-        console.error('데이터 저장 에러', err);
-    } finally {
-        if (conn) {
-            conn.release(); // 연결 반환!
-        }
-    }
-}
-
-module.exports = { saveMember, sequelize };
+module.exports = sequelize;
