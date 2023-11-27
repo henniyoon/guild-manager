76cf0b23d11359d6952557007a4a05c1e2863b23
henniyoon <2749126@naver.com>
2023-11-27 23:30:00 +0900

    본캐릭 길드와 부캐릭 길드 모델 분리

--- models/mainMember.js
+++ models/mainMember.js
@@ -23,26 +23,4 @@ const MainMember = sequelize.define('MainMember', {
     timestamps: false, // createdAt 및 updatedAt 필드 생성 안 함
 });
 
-// 부캐릭 길드 테이블 정의
-const SubMember = sequelize.define('SubMember', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    main_name: {    // 본캐릭 이름 (main_member 외래키 설정)
-        type: DataTypes.STRING,
-        allowNull: false,
-        references: {
-            model: 'main_member',
-            key: 'name',
-        },
-    },
-}, {
-    tableName: 'sub_member',
-    freezeTableName: true,
-    timestamps: false,
-});
-
-
-module.exports = { MainMember, SubMember, sequelize };
+module.exports = { MainMember, sequelize };
