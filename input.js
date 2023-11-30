06310c5f0d65bf38596f15e3f8b5299aa4f3c97f
최은철 <wndqhrgkdlel@gmail.com>
2023-11-30 16:55:29 +0900

    코드 수정

--- input.js
+++ input.js
@@ -38,42 +38,71 @@ app.use(express.json());
 // 테이블 생성 함수 호출
 createTable();
 
+// 배열을 저장할 변수
+let namesArray = [];
+
 // GET 요청 처리 - 폼 반환
 app.get('/', (req, res) => {
   res.send(`
-    <form id="memberForm" action="/addMembers" method="post">
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" required>
-      <button type="button" onclick="addMember()">Add Member</button>
-      <table id="memberTable">
-        <thead>
-          <tr>
-            <th>Names</th>
-          </tr>
-        </thead>
-        <tbody id="memberList"></tbody>
-      </table>
-      <button type="submit">Add Members</button>
-    </form>
-    <script>
-      function addMember() {
-        const nameInput = document.getElementById('name');
-        const name = nameInput.value.trim();
+  <form action="/addMembers" method="post">
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required>
+  <button type="button" onclick="addName()">Add Name</button>
+  <button type="submit">Add Members</button>
+  <ul id="nameList"></ul>
+  <input type="hidden" id="namesInput" name="names" value="">
+</form>
+<script>
+  function addName() {
+    const nameInput = document.getElementById('name');
+    const name = nameInput.value.trim();
 
-        if (name) {
-          const tableBody = document.getElementById('memberList');
-          const row = tableBody.insertRow();
-          const cell = row.insertCell(0);
-          cell.textContent = name;
+    if (name) {
+      namesArray.push(name);
+      nameInput.value = '';
 
-          nameInput.value = '';
-        }
-      }
-    </script>
+      // 추가된 이름을 화면에 표시
+      const nameList = document.getElementById('nameList');
+      const listItem = document.createElement('li');
+      listItem.textContent = name;
+      nameList.appendChild(listItem);
+    }
+  }
+
+  // submit 버튼 클릭 시 namesInput에 배열 값을 할당
+  document.querySelector('form').addEventListener('submit', function () {
+    const namesInput = document.getElementById('namesInput');
+    namesInput.value = JSON.stringify(namesArray);
+  });
+</script>
   `);
 });
 
+// POST 요청 처리
+app.post('/addMembers', async (req, res) => {
+    const { names } = req.body;
+  
+    if (!names || !Array.isArray(names) || names.length === 0) {
+      return res.status(400).json({ error: 'Names are required' });
+    }
+  
+    let conn;
+    try {
+      conn = await pool.getConnection();
+  
+      for (const name of names) {
+        await conn.query('INSERT INTO members (name) VALUES (?)', [name.trim()]);
+      }
+  
+      res.status(200).json({ message: 'Members added successfully' });
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+      if (conn) conn.end();
+    }
+  });
+
 // 서버 시작
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
