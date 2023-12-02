b4393222fe1bdba412da0c7d10a298a9c7c7ae1b
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2023-12-03 05:43:49 +0900

    input.js 모듈화 완료

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db');
-const restrictionModel = require('./models/restriction');
+const inputRoutes = require('./routes/input');
 
 const app = express();
 const port = 3030; // 사용할 포트 번호
@@ -23,66 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// GET 요청 처리 - 폼 반환
-app.get('/input', (req, res) => {
-  res.send(`
-  <form action="/addMembers" method="post" id="memberForm">
-  <label for="name">Name:</label>
-  <input type="text" id="name" name="name">
-  <button type="button" onclick="addName()">Add Name</button>
-  <button type="submit">Add Members</button>
-  <ul id="nameList"></ul>
-  <input type="hidden" id="namesInput" name="names" value="">
-</form>
-<script>
-  // names 배열 초기화
-  let namesArray = [];
-
-  function addName() {
-    const nameInput = document.getElementById('name');
-    const name = nameInput.value.trim();
-
-    if (name !== '') {
-      namesArray.push(name);
-      nameInput.value = '';
-
-      // 추가된 이름을 화면에 표시
-      const nameList = document.getElementById('nameList');
-      const listItem = document.createElement('li');
-      listItem.textContent = name;
-      nameList.appendChild(listItem);
-    }
-  }
-
-  // form submit 이벤트 시 namesInput에 배열 값을 할당
-  document.getElementById('memberForm').addEventListener('submit', function (event) {
-    const namesInput = document.getElementById('namesInput');
-    namesInput.value = JSON.stringify(namesArray);
-  });
-</script>
-  `);
-});
-
-// POST 요청 처리
-app.post('/addMembers', async (req, res) => {
-  const { names } = req.body;
-
-  if (!names || names.length === 0) {
-    return res.status(400).json({ error: '이름은 필수입니다.' });
-  }
-
-  try {
-    const namesArray = JSON.parse(names);
-
-    for (const name of namesArray) {
-      await restrictionModel.create({ name: name.trim() });
-    }
-
-    res.status(200).json({ message: '멤버가 성공적으로 추가되었습니다.' });
-  } catch (err) {
-    res.status(500).json({ error: '내부 서버 오류' });
-  }
-});
+// 라우트 설정
+app.use('/input', inputRoutes);
 
 // 서버 시작
 app.listen(port, () => {
