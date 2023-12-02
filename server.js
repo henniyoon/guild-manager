78ebdd43b934aa31ff770fe6e5a5da76ab141078
henniyoon <2749126@naver.com>
2023-12-03 08:40:30 +0900

    ejs를 사용해서 별빛 길드원 목록 출력

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const sequelize = require('./db.js');
 const MainMember = require('./models/mainMember.js');
 const SubMember = require('./models/subMember.js');
 const { findAllMembers } = require('./services/memberService.js');
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');  // EJS를 사용하도록 설정
+app.set('views', path.join(__dirname, 'views'));  // views 폴더를 템플릿 폴더로 설정
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/mainGuildList', async (req, res) => {
-    console.log('클라이언트에서 /mainGuildList에 대한 요청이 들어왔습니다.');
     try {
         await sequelize.authenticate();
         console.log('데이터베이스 연결 성공');
 
-        // 본캐 목록 조회
         const mainMembers = await findAllMembers();
 
-        // 클라이언트로 데이터 전송
-        res.setHeader('Content-Type', 'application/json'); // 수정된 부분
-        res.json(mainMembers); // 수정된 부분
+        // EJS 템플릿 사용하여 렌더링
+        res.render('mainGuildList', { mainMembers });
     } catch (error) {
         console.error('데이터베이스 조회 에러:', error);
-        res.status(500).json({ success: false, message: 'Internal Server Error' });
+        res.status(500).send('Internal Server Error');
     } finally {
         console.log('데이터베이스 연결 종료');
         sequelize.close();
@@ -30,5 +31,5 @@ app.get('/mainGuildList', async (req, res) => {
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
