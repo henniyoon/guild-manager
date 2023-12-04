dc9a0d403b95b88c8ad3f77f70d0858eaf475b02
henniyoon <2749126@naver.com>
2023-12-05 01:22:13 +0900

    파일명 변경으로 라우터 import 경로 수정, url 수정

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const guildListController = require('../controllers/guildListController.js');
+const memberListController = require('../controllers/memberListController');
 
 // 라우트 정의: 특정 URL 경로에 대한 요청을 컨트롤러로 전달
-router.get('/guildList', guildListController.getMainMemberList);
+router.get('/memberList', memberListController.getMemberList);
 
 module.exports = router;
