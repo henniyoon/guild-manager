9c614194b430bf7f656fc554f318404c1df5857a
최은철 <wndqhrgkdlel@gmail.com>
2023-12-04 13:50:03 +0900

    app.js 코드 리팩토링

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 const express = require('express');
 const path = require('path');
-const sequelize = require('./db.js');
-const routes = require('./routes/routes.js');
-
 const db = require('./db');
+const routes = require('./routes/routes');
 const inputRoutes = require('./routes/input');
 
-const app = express();
+async function startServer() {
+  const app = express();
 
+  // 뷰 엔진 및 퍼블릭 폴더 구성
+  app.set('view engine', 'ejs');
+  app.set('views', path.join(__dirname, 'views'));
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Public 폴더 내의 정적 파일 (index.html)을 제공
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
-
-// Sequelize 모델 동기화
-(async () => {
+  // Sequelize 모델 동기화
   try {
     await db.sync();
-    console.log('Sequelize models synchronized.');
+    console.log('Sequelize 모델이 동기화되었습니다.');
   } catch (error) {
-    console.error('Sequelize synchronization error:', error);
+    console.error('Sequelize 동기화 오류:', error);
   }
-})();
 
-// Express 미들웨어 설정
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  // Express 미들웨어
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-// 라우트 설정
-app.use('/input', inputRoutes);
+  // 라우트
+  app.use(routes);
+  app.use('/input', inputRoutes);
 
-// 서버 시작
-const port = process.env.PORT || 3030; // 사용할 포트 번호
-// 서버를 시작
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+  // 서버 시작
+  const port = process.env.PORT || 3030;
+  app.listen(port, () => {
+    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+  });
 
-// 에러 핸들링 미들웨어
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('문제가 발생했습니다!');
-});
+  // 에러 처리 미들웨어
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('문제가 발생했습니다!');
+  });
+}
+
+startServer();
