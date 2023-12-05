371df2efc731af971489353965968f7ed044dc27
henniyoon <2749126@naver.com>
2023-12-05 09:08:07 +0900

    updateSubMember() 메소드 추가 - 스크랩핑 해 온 sub_member 테이블에 main_name 값 추가하는 로직

--- controllers/memberListController.js
+++ controllers/memberListController.js
@@ -1,11 +1,14 @@
-const { getAllMainMembers } = require('../services/mainMemberService');
-const { getAllSubMembers } = require('../services/subMemberService');
+const { getAllMembers, getAllSubMembers } = require('../services/memberService');
 
-// 컨트롤러 로직: 데이터 조회 및 템플릿 렌더링
+const MainMember = require('../models/mainMemberModel');
+const SubMember = require('../models/subMemberModel');
+
+// 컨트롤러 로직
+//데이터 조회 및 템플릿 렌더링
 async function getMemberList(req, res) {
     try {
-        const mainMembers = await getAllMainMembers();
-        const subMembers = await getAllSubMembers();
+        const mainMembers = await getAllMembers(MainMember);
+        const subMembers = await getAllSubMembers(SubMember);
 
         res.render('memberList', { mainMembers, subMembers });
     } catch (error) {
@@ -14,4 +17,21 @@ async function getMemberList(req, res) {
     }
 }
 
-module.exports = { getMemberList };
+// sub_member 테이블의 main_name 필드 업데이트
+async function updateSubMember(req, res) {
+    try {
+        const subMemberName = req.query.subMemberName;
+        const newValue = req.query.newValue;
+
+        await SubMember.update({ main_name: newValue }, { where: { name: subMemberName } });
+        res.status(200).json({ success: true, message: '본캐 추가 성공'});
+  } catch (error) {
+    console.error('본캐 추가 중 오류 발생:', error);
+    res.status(500).json({ success: false, message: '본캐 추가 실패' });
+  }
+};
+
+module.exports = { 
+    getMemberList,
+    updateSubMember
+};
