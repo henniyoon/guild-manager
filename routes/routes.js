b86cbcfdce3a5493d4384ad4e5dd1497105c2c74
henniyoon <2749126@naver.com>
2023-12-05 09:08:23 +0900

    /updateSubMember 라우팅

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,7 @@ const memberListController = require('../controllers/memberListController');
 // 라우트 정의: 특정 URL 경로에 대한 요청을 컨트롤러로 전달
 router.get('/memberList', memberListController.getMemberList);
 
+// sub_member 테이블에 main_name 업데이트를 위한 엔드포인트 추가
+router.post('/updateSubMember', memberListController.updateSubMember);
+
 module.exports = router;
