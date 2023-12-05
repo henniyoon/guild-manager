49ba59d094269f2ad1fa1542e1604f55cdea9949
최은철 <wndqhrgkdlel@gmail.com>
2023-12-05 11:39:02 +0900

    타임스탬프 기능 추가

--- models/restriction.js
+++ models/restriction.js
@@ -1,17 +1,20 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db.js');
 
-// 본캐릭 길드 테이블 정의
 const restriction = sequelize.define('restriction', {
     name: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true, // 중복되지 않도록 설정
     },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
 }, {
     tableName: 'restriction', // 실제 테이블 이름
     freezeTableName: true, // 테이블 이름 고정
-    timestamps: false, // createdAt 및 updatedAt 필드 생성 안 함
+    timestamps: true,
 });
 
 module.exports = restriction;
