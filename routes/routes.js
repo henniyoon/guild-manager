5c756074429a5853940788e7bd053f977a8fa0da
최은철 <wndqhrgkdlel@gmail.com>
2023-12-05 16:16:14 +0900

    /nobleLimit 모듈화

--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const memberListController = require("../controllers/memberListController");
-const { findNobleLimit } = require("../findNobleLimit");
 const nobleLimit = require("../models/nobleLimit");
+const NobleLimitController = require("../controllers/nobleLimitController")
 
 // 라우트 정의: 특정 URL 경로에 대한 요청을 컨트롤러로 전달
 router.get("/memberList", memberListController.getMemberList);
 
-router.get("/nobleLimit", async (req, res) => {
-  try {
-    const results = await findNobleLimit();
-
-    // main_name만 추출
-    const mainNames = results.map((result) => result.main_name);
-
-    // EJS 템플릿 렌더링
-    res.render("nobleLimit", { mainNames });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "내부 서버 오류" });
-  }
-});
+router.get("/nobleLimit", NobleLimitController.NobleLimitController);
 
 router.get("/input", (req, res) => {
   res.send(`
