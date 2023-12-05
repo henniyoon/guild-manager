078c448a2074121f056d8b13a50e031e3804772e
최은철 <wndqhrgkdlel@gmail.com>
2023-12-05 12:50:05 +0900

    시간은 저장되지 않도록 수정

--- models/restriction.js
+++ models/restriction.js
@@ -8,7 +8,7 @@ const restriction = sequelize.define('restriction', {
         unique: false,
     },
     createdAt: {
-        type: DataTypes.DATE,
+        type: DataTypes.DATEONLY,
         allowNull: true,
     },
 }, {
