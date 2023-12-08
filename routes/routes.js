b73060029864c950afc4fae868ac75435f981955
henniyoon <2749126@naver.com>
2023-12-08 15:30:20 +0900

    /memberSync 동기화 기능 라우팅

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const guildSettingController = require("../controllers/guildSettingController");
 const memberListController = require("../controllers/memberListController");
+const memberSyncController = require("../controllers/memberSyncController");
 const NobleLimitController = require("../controllers/nobleLimitController");
 const inputController = require("../controllers/inputController");
 const addMemberController = require("../controllers/addMemberController");
@@ -17,16 +18,20 @@ router.get("/guildSetting", guildSettingController.guildSettingForm);
 
 router.get("/nobleLimit", NobleLimitController.NobleLimitController);
 
+
 router.get("/input", inputController.inputController);
 
 router.get("/findLimit", findLimit)
 
 router.post("/addMembers", addMemberController.addMemberController);
 
-// 부캐릭 테이블에 본캐릭 정보 추가
-router.post("/updateSubMember", memberListController.updateSubMember);
-
 // 길드 정보 업데이트
 router.post("/updateGuildSetting", guildSettingController.updateGuildInfo);
+
+// 길드원 동기화 (메이플 랭킹 페이지에서 스크래핑)
+router.post("/memberSync", memberSyncController.syncMemberList);
+
+// 부캐릭 테이블에 본캐릭 정보 추가
+router.post("/updateSubMember", memberListController.updateSubMember);
 
 module.exports = router;
