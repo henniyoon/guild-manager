852a18f574c8523ae17130cb23c8f65f745d5a41
henniyoon <2749126@naver.com>
2023-12-08 15:29:14 +0900

    main.js에 있던 fetAndSaveMembers 모듈화

--- services/dataService.js
+++ services/dataService.js
@@ -1,10 +1,15 @@
 const { fetchData } = require('../utils/scraping.js');
+const {
+  saveMembersIfNotExist, 
+  deleteMembersNotInList, 
+  getAllMembers 
+}= require('../services/memberService');
 
 // 비즈니스 로직: 길드 데이터 스크래핑
-async function fetchGuildData(guildId, numPages) {
+async function fetchGuildData(guildId) {
   const promises = [];
 
-  for (let i = 1; i <= numPages; i++) {
+  for (let i = 1; i <= 10; i++) {
     const url = `https://maplestory.nexon.com/Common/Guild?gid=${guildId}&wid=0&orderby=1&page=${i}`;
     promises.push(fetchData(url));
   }
@@ -13,4 +18,11 @@ async function fetchGuildData(guildId, numPages) {
   return characterNames.flat(); // 2D 배열을 1D 배열로 평탄화
 }
 
-module.exports = { fetchGuildData };
+// 비즈니스 로직: 길드 데이터 스크래핑 및 멤버 저장
+async function fetchAndSaveMembers(guildId, MemberModel) {
+    const characterNames = await fetchGuildData(guildId);
+    await saveMembersIfNotExist(characterNames, MemberModel);
+    await deleteMembersNotInList(characterNames, MemberModel);
+}
+
+module.exports = { fetchAndSaveMembers };
