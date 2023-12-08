2556e174dcd785caa5d9d396ce24224011ff8ff0
henniyoon <2749126@naver.com>
2023-12-08 15:28:42 +0900

    길드원 스크래핑 해오는 길드 명부 동기화 버튼 추가

--- public/js/memberListActions.js
+++ public/js/memberListActions.js
@@ -1,8 +1,25 @@
-function goToGuildSetting() {
+async function goToGuildSetting() {
   window.location.href = '/guildSetting';
 }
 
-function toggleEditable(cell, subMemberName) {
+async function goToMemberSync() {
+   // 서버에 업데이트 요청을 보내는 코드
+   const response = await fetch(`/memberSync`, {
+    method: 'POST',
+  });
+
+  // 서버에서 온 응답을 확인하고 필요한 작업을 수행
+  if (response.ok) {
+    console.log('길드원 동기화 성공');
+    
+    // 리다이렉트 후에 새로고침
+    window.location.reload();
+  } else {
+    console.error('길드원 동기화 실패');
+  }
+}
+
+async function toggleEditable(cell, subMemberName) {
   const input = cell.querySelector('input');
   const displayValue = cell.querySelector('.display-value');
 
