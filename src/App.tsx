1fbdaca2daac8cd96ae7c7876a900fa3f03f22f2
최은철 <wndqhrgkdlel@gmail.com>
2024-01-29 18:26:41 +0900

    파일명 수정 ResultPage.tsx -> Guildpage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Mainpage from './component/Mainpage';
-import ResultPage from './component/ResultPage';
+import Guildpage from './component/Guildpage';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Mainpage />} />
-        <Route path="/result" element={<ResultPage />} />
+        <Route path="/result" element={<Guildpage />} />
       </Routes>
     </Router>
   );
