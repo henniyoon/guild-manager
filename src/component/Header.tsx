dde1da6a857fce80579e5a9665de6faaabf99d69
최은철 <wndqhrgkdlel@gmail.com>
2024-01-29 12:48:16 +0900

    기본적인 헤더

---
+++ src/component/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../style/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">LOGO</div>
+      <h1 className="title">My Website</h1>
+      <nav className="nav">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/services">Services</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
