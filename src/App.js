db16137a9bb7cc63ea7c3892c851e08d0f1c965a
최은철 <wndqhrgkdlel@gmail.com>
2024-01-29 18:21:48 +0900

    Header 추가, css 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Mainpage from './component/Mainpage';
 import ResultPage from './component/ResultPage';
 
