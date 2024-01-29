75d95b116af2c45bcae0fbea33e3343522ac5ac1
최은철 <wndqhrgkdlel@gmail.com>
2024-01-29 16:15:45 +0900

    GET요청, 잘 안됨

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+app.get('/guild', (req, res) => {
+  const { server, input } = req.query;
+  
+  // 여기에서 server와 input 값에 대한 로직을 처리합니다.
+  console.log(`서버: ${server}, 입력값: ${input}`);
+
+  // 처리 결과를 클라이언트에 응답으로 보냅니다.
+  res.json({ message: `서버: ${server}, 입력값: ${input}을(를) 받았습니다.` });
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
