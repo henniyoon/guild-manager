ceff774dcd6a00544d52931cf7fcfa657c0a4ca7
최은철 <wndqhrgkdlel@gmail.com>
2024-01-29 10:31:40 +0900

    js 폴더 잠시 빼놓기

--- javascript/db.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require('sequelize');
-const config = require('./config/config.json');
-
-const env = process.env.NODE_ENV || 'development';
-const sequelize = new Sequelize(config[env]);
-
-module.exports = sequelize;
