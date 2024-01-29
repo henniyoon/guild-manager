2ac6f0b61313e5ebe7e5f4a54cfc09248512d41d
최은철 <wndqhrgkdlel@gmail.com>
2024-01-29 19:10:24 +0900

    캐릭터 정보 조회

--- src/component/ApiTest.js
+++ src/component/ApiTest.js
@@ -2,8 +2,8 @@ const API_KEY = "test_30c434a462a6ed7731bdbb00b7c646320cf57614f257e894ce568d5c72
 const characterName = "헨쁘";
 const date = "2023-12-21"
 const urlString = "https://open.api.nexon.com/heroes/v1/id?character_name=" + characterName + date;
-
-const url = "https://open.api.nexon.com/maplestory/v1/id?character_name=%EB%A9%94%EB%AA%A8%EB%9D%BC%EC%9D%B4%EC%A5%AC"
+// { ocid: '9bed2e0f6289cb04492e68706e3baad8' }
+const url = "https://open.api.nexon.com/maplestory/v1/character/basic?ocid=9bed2e0f6289cb04492e68706e3baad8&date=2023-12-21"
 
 const answer = fetch(url, {
     headers:{
