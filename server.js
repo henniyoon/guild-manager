13299ca30c667f282d14b25fa3ae062700610118
최은철 <wndqhrgkdlel@gmail.com>
2024-01-30 12:41:50 +0900

    mairaDB 연결

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mariadb  = require('mariadb');
 const cors = require('cors');
 
 app.use(cors());
@@ -10,6 +11,28 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '0000',
+  database: 'guild_manager',
+  connectionLimit: 5
+});
+
+async function connectDB() {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    console.log("데이터베이스 연결 성공");
+  } catch (err) {
+    console.error("데이터베이스 연결 실패:", err.message);
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+connectDB();
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
