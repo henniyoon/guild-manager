f3699660ed142432855398f0c76e1d8ef92c29ad
henniyoon <2749126@naver.com>
2024-02-01 05:59:39 +0900

    애플리케이션의 최상위 컴포넌트로 BroserRouter 컴포넌트 이동

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Header from './component/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
+    <BrowserRouter>
+      <Header />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
