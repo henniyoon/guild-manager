259941eca24f4a0c707bb7fa89a18fe108c6c365
henniyoon <2749126@naver.com>
2024-02-01 05:59:39 +0900

    애플리케이션의 최상위 컴포넌트로 BroserRouter 컴포넌트 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Mainpage from './component/Mainpage';
 import Guildpage from './component/Guildpage';
 import Graphpage from './component/Graphpage';
@@ -7,14 +7,12 @@ import Adminpage from './component/Adminpage'
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/Guildpage" element={<Guildpage />} />
-        <Route path="/Graphpage" element={<Graphpage />} />
-        <Route path="/Adminpage" element={<Adminpage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Mainpage />} />
+      <Route path="/Guildpage" element={<Guildpage />} />
+      <Route path="/Graphpage" element={<Graphpage />} />
+      <Route path="/Adminpage" element={<Adminpage />} />
+    </Routes>
   );
 };
 
