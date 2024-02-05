13cfa5f35eeaf09e0229206066e82d02dca2bda5
최은철 <wndqhrgkdlel@gmail.com>
2024-02-05 19:56:14 +0900

    로그인시 이름 뜨게 수정

--- client/src/component/Login.tsx
+++ client/src/component/Login.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { useAuth } from '../Context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 
 interface LoginProps {
   // 필요한 경우 추가 props 정의
+}
+
+interface decodedToken {
+  username : string;
 }
 
 const Login: React.FC<LoginProps> = () => {
@@ -34,7 +39,9 @@ const Login: React.FC<LoginProps> = () => {
       const data = await response.json();
       if (response.ok) {
         console.log('로그인 성공:', data);
-        login({ username: data.username }); // 로그인 상태 업데이트
+        const decodedToken:decodedToken = jwtDecode(data.token);
+        console.log(decodedToken)
+        login({ username: decodedToken.username }); // 로그인 상태 업데이트
         localStorage.setItem('token', data.token); // 토큰 저장
         navigate('/'); // 홈페이지로 리다이렉트
       } else {
