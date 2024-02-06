3cf2c241cef356479ed85e60655de9a014c8be32
최은철 <wndqhrgkdlel@gmail.com>
2024-02-06 13:01:55 +0900

    주에 따른 조회

--- server/server.js
+++ server/server.js
@@ -41,13 +41,25 @@ sequelize.authenticate()
 
 // 노블 제한 기록 조회 API
 app.get('/api/records', async (req, res) => {
+  // 쿼리스트링에서 'week' 파라미터 값을 가져옴
+  const week = req.query.week;
+  console.log(week)
+
   try {
-    const rows = await Record.findAll();
-    res.json(rows);
-  } catch (err) {
-    console.error("데이터베이스 쿼리 실행 실패:", err.message);
-    res.status(500).send('서버 오류 발생');
-  } 
+    // 'week' 필드가 쿼리스트링에서 받은 'week' 값과 일치하는 Record 데이터를 조회
+    const records = await Record.findAll({
+      where: {
+        week: week
+      }
+    });
+
+    // 조회된 데이터를 JSON 형태로 응답
+    res.json(records);
+  } catch (error) {
+    // 에러 처리
+    console.error("데이터를 불러오는 데 실패했습니다:", error);
+    res.status(500).send("서버 에러 발생");
+  }
 });
 
 // 노블 제한 기록 업데이트 API
