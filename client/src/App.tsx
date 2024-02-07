84f514d5193b91a99cca451b95ee2e855a77887a
henniyoon <2749126@naver.com>
2024-02-07 14:01:45 +0900

    오류 나던거 해결!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import Mainpage from '../src/component/Mainpage';
 import Guildpage from '../src/component/Guildpage';
 import Graphpage from '../src/component/Graphpage';
 import Adminpage from '../src/component/Adminpage'
-// import Loginpage from '../src/component/Loginpage';
+import Loginpage from '../src/component/Loginpage';
 import SignUppage from '../src/component/SignUppage';
 
 const App: React.FC = () => {
@@ -14,7 +14,7 @@ const App: React.FC = () => {
       <Route path="/Guildpage/:server/:guild" element={<Guildpage />} />
       <Route path="/Graphpage/:memberName" element={<Graphpage />} />
       <Route path="/Adminpage" element={<Adminpage />} />
-      {/* <Route path="/Login" element={<Loginpage />} /> */}
+      <Route path="/Login" element={<Loginpage />} />
       <Route path="/SignUp" element={<SignUppage />} />
     </Routes>
   );
