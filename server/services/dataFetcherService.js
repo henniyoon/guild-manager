fb9a14b4804c65e1b8d344008aa58ca2968893eb
henniyoon <2749126@naver.com>
2024-02-07 11:04:13 +0900

    길드 정보 조회 후 guilds DB 저장, 길드원 목록 characters DB 저장

--- server/services/dataFetcherService.js
+++ server/services/dataFetcherService.js
@@ -62,9 +62,80 @@ async function guildData(guild, world_name) {
             console.error('api 조회 db 저장 에러:', error);
             throw new Error('서버 에러');
         }
-    }
+    };
+    // 이 API 조회 전에 'last_updated가 조회일 오전 1시 이전이면' 이라는 조건 필요!
+    const currentDate = new Date();                 // 현재 날짜 객체 생성
+    currentDate.setDate(currentDate.getDate() - 1); // 어제 날짜로 설정 (하루를 밀리초 단위로 계산하여 뺌)
+    const formattedDate = currentDate.toISOString().split('T')[0];  // 날짜를 YYYY-MM-DD 형식으로 포매팅
 
-    
+    const guildData = await Guild.findOne({
+        where: {
+            name: guild,
+            world_id: world.id,
+        },
+    });
+
+    const guild_data_url = `${API_BASE_URL}${GUILD_BASIC_ENDPOINT}?oguild_id=${guildData.oguild_id}&date=${formattedDate}`;
+    try {
+        // API 조회
+        const apiResponse = await axios.get(guild_data_url, {
+            headers: {
+                "x-nxopen-api-key": API_KEY,
+            },
+        });
+        const apiData = apiResponse.data;
+
+        await Guild.update(
+            {
+                master_name: apiData.guild_master_name,
+                member_count: apiData.guild_member_count,
+                level: apiData.guild_level,
+                noblesse_skill_level: apiData.guild_noblesse_skill.reduce((sum, skill) => sum + skill.skill_level, 0),
+                guild_mark: apiData.guild_mark,
+                guild_mark_custom: apiData.guild_mark_custom,
+                last_updated: new Date(),
+            },
+            {
+                where: { id: guildData.id },
+            }
+        );
+        // console.log("Entering for loop");
+        for (const characterName of apiData.guild_member) {
+            const characterExists = await Character.findOne({ where: { name: characterName } });
+
+            if (characterExists) {
+                // 이미 존재하는 경우 업데이트
+                await characterExists.update(
+                    {
+                        guild_id: guildData.id,
+                    },
+                    {
+                        where: { name: characterName },
+                    }
+                );
+                // console.log(`${characterName} 업데이트 완료`);
+            } else {
+                await Character.create({
+                    guild_id: guildData.id,
+                    name: characterName,
+                    // class: ,
+                    // level:,
+                    // image:,
+                    // last_updated:,
+                });
+            };
+        };
+        console.log("업데이트 완료");
+
+        
+
+
+        // console.log("apiData:", apiData);
+    } catch (error) {
+        console.error('api 조회 db 저장 에러:', error);
+        throw new Error('서버 에러');
+    };
+
 
 };
 
