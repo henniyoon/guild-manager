b0518694ea430f78ccfa42f98d13905f404be9ca
최은철 <wndqhrgkdlel@gmail.com>
2024-02-08 13:01:24 +0900

    헷갈려서 바꿈

--- server/routes/recordRoutes.js
+++ server/routes/recordRoutes.js
@@ -3,16 +3,16 @@ const recordRouter = express.Router();
 
 const RecordController = require('../controllers/recordController.js');
 
-// 노블 제한 기록 조회
-recordRouter.get('/records', RecordController.getRecordsController);
+// 주간 점수 기록 조회
+recordRouter.get('/records', RecordController.test);
 
-// 노블 제한 기록 추가
+// 주간 점수 기록 추가
 recordRouter.post('/records', RecordController.addRecordController);
 
-// 노블 제한 기록 수정
+// 주간 점수 기록 수정
 recordRouter.post('/updateRecords', RecordController.updateRecordsController);
 
-// 노블 제한 기록 삭제
+// 주간 점수 기록 삭제
 recordRouter.delete('/deleteRecord/:id', RecordController.deleteRecordsController)
 
 module.exports = recordRouter;
