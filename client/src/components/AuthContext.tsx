ed32a7f5e11c3e8c7f4d63be2070cb85accc8fc4
최은철 <wndqhrgkdlel@gmail.com>
2024-02-08 14:33:00 +0900

    새로고침해도 로그인상태 유지

--- client/src/components/AuthContext.tsx
+++ client/src/components/AuthContext.tsx
@@ -1,6 +1,6 @@
 // * useContext 훅과 AuthProvider를 사용하여 로그인 정보(인증상태)를 전역으로 공유할 수 있도록 해주는 컴포넌트
 
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 
 interface AuthContextType {
   isLoggedIn: boolean;
@@ -21,13 +21,24 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userInfo, setUserInfo] = useState<{ username?: string } | null>(null);
 
+  useEffect(() => {
+    // 클라이언트 측 저장소에서 로그인 정보 확인
+    const storedUserInfo = localStorage.getItem('userInfo');
+    if (storedUserInfo) {
+      setUserInfo(JSON.parse(storedUserInfo));
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const login = (user: { username: string }) => {
+    localStorage.setItem('userInfo', JSON.stringify(user));
     setIsLoggedIn(true);
     setUserInfo(user);
   };
 
   const logout = () => {
-    setIsLoggedIn(false);
+    localStorage.removeItem('userInfo');
+      setIsLoggedIn(false);
     setUserInfo(null);
   };
 
