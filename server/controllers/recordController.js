7454b302125350642f14f3d385531ccc550f85bc
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2024-02-10 23:09:36 +0900

    길드 정보 표시

--- server/controllers/recordController.js
+++ server/controllers/recordController.js
@@ -1,28 +1,40 @@
 const RecordService = require('../services/recordService.js');
 const Record = require('../models/Record.js');
+const Guild = require('../models/Guild.js');
 const jwt = require('jsonwebtoken');
 
 const getRecordsController = async (req, res) => {
     const week = req.query.week;
-    
-    // 요청 헤더에서 토큰 추출
     const token = req.headers.authorization?.split(' ')[1]; // "Bearer TOKEN" 형식 가정
 
-    // 토큰이 없는 경우 오류 처리
     if (!token) {
         return res.status(401).send("토큰이 필요합니다.");
     }
 
     try {
-        // 토큰 검증
+        // 토큰 검증 및 디코딩
         const decoded = jwt.verify(token, "WE_MUST_HIDE_THIS_KEY");
-        console.log(decoded)
-        // 검증 성공 후, 데이터베이스 조회
-        const records = await RecordService.getRecords(week);
 
+        // 길드 정보 조회
+        const guildInfo = await Guild.findOne({
+            where: {
+                world_id: decoded.guild_world_id,
+                name: decoded.guild_name,
+            },
+        });
+
+        if (!guildInfo) {
+            return res.status(404).send("해당하는 길드를 찾을 수 없습니다.");
+        }
+
+        // 길드 정보를 사용하여 더 많은 로직을 처리할 수 있습니다.
+        // 예: decoded의 정보를 바탕으로 records 조회
+        const records = await RecordService.getRecords(week);
+console.log(`11111111111 : `,guildInfo)
         const response = records.map(record => ({
             ...record.toJSON(),
-            character_name: record.character?.name
+            character_name: record.character?.name,
+            guild_info: guildInfo // 길드 정보 추가
         }));
 
         res.json(response);
@@ -32,16 +44,14 @@ const getRecordsController = async (req, res) => {
             console.error("토큰이 만료되었습니다:", error);
             return res.status(401).send("토큰이 만료되었습니다. 다시 로그인해주세요.");
         } else if (error.name === 'JsonWebTokenError') {
-            // 다른 JWT 오류 처리
             console.error("유효하지 않은 토큰입니다:", error);
             return res.status(401).send("유효하지 않은 토큰입니다.");
         } else {
-            // 기타 서버 오류 처리
-            console.error("Records 테이블을 불러오는 데 실패했습니다:", error);
+            console.error("서버 에러", error);
             return res.status(500).send("서버 에러");
         }
     }
-}
+};
 
 const addRecordController = async (req, res) => {
     try {
