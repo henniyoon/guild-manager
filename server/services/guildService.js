929050df1a35b61122dbde2d4d123e7e5187f34b
henniyoon <2749126@naver.com>
2024-02-11 15:38:04 +0900

    guilds 테이블의 last_updated 값을 api에서 받아온 date로 변경

--- server/services/guildService.js
+++ server/services/guildService.js
@@ -1,24 +1,31 @@
 const Guild = require('../models/Guild.js');
-const Character = require('../models/Character.js');
 const WorldService = require('./worldService.js');
 const GuildAPIService = require('./guildApiService.js');
 
+async function getGuild(guildName, worldId) {
+    const guild = await Guild.findOne({ where: { name: guildName, world_id: worldId } });
+    return guild;
+}
+
 async function getGuildId(guildName, worldId) {
     const guild = await Guild.findOne({ where: { name: guildName, world_id: worldId } });
-    return guild.id;
+    return guild ? guild.id : null;
 }
 
 async function createGuild(guild, worldName) {
     // 해당 월드의 월드 번호 조회
     const worldId = await WorldService.getWordId(worldName);
-    console.log("worldId: ", worldId);
     try {
         let apiData = await GuildAPIService.getOguildId(guild, worldName);
         apiData = {
             ...apiData,
             ... await GuildAPIService.getGuildBasicData(apiData.oguild_id)
         };
-        // console.log("apiData:", apiData);
+    
+        const guildMembers = apiData.guild_member;
+
+        const apiDate = new Date(apiData.date);
+        apiDate.setHours(apiDate.getHours() + 9);
 
         await Guild.create({
             world_id: worldId,
@@ -30,10 +37,12 @@ async function createGuild(guild, worldName) {
             noblesse_skill_level: apiData.guild_noblesse_skill.reduce((sum, skill) => sum + skill.skill_level, 0),
             guild_mark: apiData.guild_mark,
             guild_mark_custom: apiData.guild_mark_custom,
-            last_updated: new Date(),
+            last_updated: apiDate,
 
         });
         console.log("길드 정보 추가 성공");
+
+        return guildMembers;
 
     } catch (error) {
         console.error('에러 발생:', error);
@@ -50,19 +59,20 @@ async function updateGuild(guild, worldName) {
             ...apiData,
             ... await GuildAPIService.getGuildBasicData(apiData.oguild_id)
         };
-        // console.log("apiData:", apiData);
+
+        const apiDate = new Date(apiData.date);
+        apiDate.setHours(apiDate.getHours() + 9);
 
         await Guild.update({
                 world_id: worldId,
                 name: guild,
-                oguild_id: apiData.oguild_id,
                 master_name: apiData.guild_master_name,
                 member_count: apiData.guild_member_count,
                 level: apiData.guild_level,
                 noblesse_skill_level: apiData.guild_noblesse_skill.reduce((sum, skill) => sum + skill.skill_level, 0),
                 guild_mark: apiData.guild_mark,
                 guild_mark_custom: apiData.guild_mark_custom,
-                last_updated: new Date(),
+                last_updated: apiDate,
             },
             { where: { oguild_id: apiData.oguild_id } }
         );
@@ -75,7 +85,10 @@ async function updateGuild(guild, worldName) {
 }
 
 module.exports = {
+    getGuild,
     getGuildId,
     createGuild,
     updateGuild,
 };
+
+// createGuild("리더", "스카니아");
