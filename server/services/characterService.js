d221f5a35af0078d94ffbc87a985eaf5517bdbe3
henniyoon <2749126@naver.com>
2024-02-11 15:39:26 +0900

    character update 코드 추가

--- server/services/characterService.js
+++ server/services/characterService.js
@@ -13,9 +13,9 @@ async function createCharacter(characterName) {
         };
         console.log("apiData:", apiData);
 
+        const guildName = apiData.character_guild_name;
         const worldId = await WorldService.getWordId(apiData.world_name);
-        const guild = apiData.character_guild_name;
-        const guildId = await GuildService.getGuildId(guild, worldId);
+        const guildId = await GuildService.getGuildId(guildName, worldId);
         
         await Character.create({
             guild_id: guildId,
@@ -24,8 +24,38 @@ async function createCharacter(characterName) {
             class: apiData.character_class,
             level: apiData.character_level,
             image: apiData.character_image,
-            last_updated: apiData.date,
+            last_updated: new Date(),
         });
+        console.log("캐릭터 정보 추가 성공");
+
+    } catch (error) {
+        console.error('에러 발생:', error);
+        throw new Error('서버 에러');
+    }
+}
+
+async function updateCharacter(characterName) {
+    try {
+        let apiData = await GuildAPIService.getCharacterOcid(characterName);
+        apiData = {
+            ...apiData,
+            ... await GuildAPIService.getCharacterBasicData(apiData.ocid)
+        };
+        console.log("apiData:", apiData);
+
+        const guildName = apiData.character_guild_name;
+        const worldId = await WorldService.getWordId(apiData.world_name);
+        const guildId = await GuildService.getGuild(guildName, worldId);
+        
+        await Character.update({
+            guild_id: guildId,
+            name: characterName,
+            class: apiData.character_class,
+            level: apiData.character_level,
+            image: apiData.character_image,
+            last_updated: new Date(),
+        }, { where: { ocid: apiData.ocid } }
+        );
         console.log("캐릭터 정보 추가 성공");
 
     } catch (error) {
@@ -36,4 +66,5 @@ async function createCharacter(characterName) {
 
 module.exports = {
     createCharacter,
+    updateCharacter
 }
