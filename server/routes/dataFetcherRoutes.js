9ae226f4cd5816cd8818bf865acc26443edd5da9
henniyoon <2749126@naver.com>
2024-02-14 11:59:34 +0900

    길드원 정보를 받아오는 controller 라우팅

--- server/routes/dataFetcherRoutes.js
+++ server/routes/dataFetcherRoutes.js
@@ -5,6 +5,7 @@ const DataFetcherController = require('../controllers/dataFetcherController.js')
 
 
 dataFetcherRouter.get('/Guild/:worldName/:guildName', DataFetcherController.getGuildController);
+dataFetcherRouter.get('/GuildMembers/:worldName/:guildName', DataFetcherController.getCharacterController);
 dataFetcherRouter.post('/GuildPage/:worldName/:guildName', DataFetcherController.dataFetcherController);
 
 module.exports = dataFetcherRouter;
