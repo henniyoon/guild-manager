0b6386123e10521eafd07453110db832cac20098
henniyoon <2749126@naver.com>
2024-02-14 17:30:03 +0900

    부트스트랩 css import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
 import { AuthProvider } from './components/AuthContext';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
