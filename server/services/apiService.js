1eea6d10f2e2f58185d1bf5f5b7386e07e2b56ec
henniyoon <2749126@naver.com>
2024-02-15 20:20:05 +0900

    월드명과 ocid로 캐릭터 유니온 랭킹 조회하는 getMainCharacterName 추가

--- server/services/apiService.js
+++ server/services/apiService.js
@@ -7,12 +7,12 @@ const config = JSON.parse(configData);
 const API_KEY = config.apiKey;
 
 const API_BASE_URL = "https://open.api.nexon.com/maplestory/v1";
-const UNION_RANKING_ENDPOINT = "/ranking/union";
 
 const currentDate = new Date();
 currentDate.setDate(currentDate.getDate() - 1);
 const formattedDate = currentDate.toISOString().split('T')[0];
 
+// nexon open api 조회 
 async function getApiResponse(url) {
     try {
         const response = await axios.get(url, { headers: { "x-nxopen-api-key": API_KEY } });
@@ -25,26 +25,42 @@ async function getApiResponse(url) {
 
 // 길드명, 월드명으로 oguild_id 조회
 async function getOguildId(guild, worldName) {
-    const guildIdUrl = `${API_BASE_URL}/guild/id?guild_name=${encodeURIComponent(guild)}&world_name=${encodeURIComponent(worldName)}`;
+    const guildIdUrl = `${API_BASE_URL}/guild/id
+    ?guild_name=${encodeURIComponent(guild)}&world_name=${encodeURIComponent(worldName)}`;
+
     return await getApiResponse(guildIdUrl);
 }
 
 // oguild_id로 길드 정보 조회
 async function getGuildBasicData(oguildId) {
-    const guildDataUrl = `${API_BASE_URL}/guild/basic?oguild_id=${oguildId}&date=${formattedDate}`;
+    const guildDataUrl = `${API_BASE_URL}/guild/basic
+    ?oguild_id=${oguildId}&date=${formattedDate}`;
+
     return await getApiResponse(guildDataUrl);
 }
 
 // 캐릭터명으로 ocid 조회
 async function getCharacterOcid(characterName) {
-    const characterOcidUrl = `${API_BASE_URL}/id?character_name=${encodeURIComponent(characterName)}`;
+    const characterOcidUrl = `${API_BASE_URL}/id
+    ?character_name=${encodeURIComponent(characterName)}`;
+
     return await getApiResponse(characterOcidUrl);
 }
 
 // ocid로 캐릭터 정보 조회
 async function getCharacterBasicData(ocid) {
-    const characterDataUrl = `${API_BASE_URL}/character/basic?ocid=${encodeURIComponent(ocid)}&date=${formattedDate}`;
+    const characterDataUrl = `${API_BASE_URL}/character/basic
+    ?ocid=${encodeURIComponent(ocid)}&date=${formattedDate}`;
+
     return await getApiResponse(characterDataUrl);
+}
+
+// 월드명과 ocid로 캐릭터 유니온 랭킹 조회 (본캐 찾기)
+async function getMainCharacterName(worldName, ocid) {
+    const unionRankingUrl = `${API_BASE_URL}/ranking/union
+    ?date=${formattedDate}&world_name=${encodeURIComponent(worldName)}&ocid=${encodeURIComponent(ocid)}`;
+
+    return await getApiResponse(unionRankingUrl);
 }
 
 module.exports = {
@@ -52,4 +68,5 @@ module.exports = {
     getGuildBasicData,
     getCharacterOcid,
     getCharacterBasicData,
+    getMainCharacterName,
 };
