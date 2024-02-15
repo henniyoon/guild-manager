3700cea5aa2ac3e8c8ffa09e6cdc16be2db2fa80
최은철 <wndqhrgkdlel@gmail.com>
2024-02-15 18:30:50 +0900

    대충 컴포넌트 하나

--- client/src/pages/GraphPage/Graphpage.tsx
+++ client/src/pages/GraphPage/Graphpage.tsx
@@ -1,40 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Bar } from 'react-chartjs-2';
-import axios from 'axios';
+import Chart from "./components/Chart";
 
 const GraphPage = () => {
-    const [chartData, setChartData] = useState({});
-
-    const loadChartData = async () => {
-        const response = await axios.get('/api/records'); // API 엔드포인트 경로는 실제 환경에 맞게 조정
-        const records = response.data;
-
-        // 데이터 가공
-        const labels = records.map((record: { week: any; }) => record.week);
-        const data = records.map((record: { weekly_score: any; }) => record.weekly_score);
-
-        setChartData({
-            labels,
-            datasets: [
-                {
-                    label: '주간 점수',
-                    data,
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)',
-                    borderWidth: 1,
-                },
-            ],
-        });
-    };
-
-    useEffect(() => {
-        loadChartData();
-    }, []);
-
     return (
         <div>
-            <h2>유저의 기록을 그래프로 보여주기</h2>
-            <Bar data={chartData} options={{ responsive: true }} />
+            <Chart />
         </div>
     );
 };
