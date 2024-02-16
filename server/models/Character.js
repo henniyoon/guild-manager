65553f0551ff1a53e220be506eaf786f6c62cd0b
henniyoon <2749126@naver.com>
2024-02-16 16:48:12 +0900

    characters 테이블에 world_id 필드 추가

--- server/models/Character.js
+++ server/models/Character.js
@@ -2,6 +2,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db.js');
 const Guild = require('./Guild.js');
+const World = require('./World.js');
 
 const Characters = sequelize.define('Characters', {
   id: {         // index
@@ -9,6 +10,14 @@ const Characters = sequelize.define('Characters', {
     primaryKey: true,
     autoIncrement: true,
     allowNull: false,
+  },
+  world_id: {       // 월드 번호 (worlds 테이블의 id를 외래키로 사용)
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: World,
+      key: 'id',
+    },
   },
   guild_id: {       // 길드 번호 (guilds 테이블의 id를 외래키로 사용)
     type: DataTypes.INTEGER,
