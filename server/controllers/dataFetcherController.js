77c84913efea7d011239005d5f4d202a9dd00c5a
henniyoon <2749126@naver.com>
2024-02-16 16:47:48 +0900

    기존에 있던 비즈니스 로직 삭제 - guildPageService.js로 이동

--- server/controllers/dataFetcherController.js
+++ server/controllers/dataFetcherController.js
@@ -1,13 +1,13 @@
 const WorldService = require('../services/worldService.js');
 const GuildService = require('../services/guildService.js');
 const CharacterService = require('../services/characterService.js');
+const GuildPageService = require('../services/guildPageService.js');
 
 async function getGuildController(req, res) {
     const guildName = req.params.guildName;
     const worldName = req.params.worldName;
     try {
-        const wolrdId = await WorldService.getWordId(worldName);
-        const guild = await GuildService.getGuild(guildName, wolrdId);
+        const guild = await GuildService.getGuild(guildName, worldName);
 
         if (guild) {
             res.json(guild);
@@ -45,41 +45,10 @@ async function dataFetcherController(req, res) {
     const worldName = req.params.worldName;
 
     try {
-        const wolrdId = await WorldService.getWordId(worldName);
-        const guildExists = await GuildService.getGuild(guildName, wolrdId);
-
-        if (!guildExists) {
-            // Guild가 존재하지 않으면 생성
-            const guildMembers = await GuildService.createGuild(guildName, worldName);
-            for (const guildMember of guildMembers) {
-                await CharacterService.createCharacter(guildName, worldName, guildMember);
-            };
-            res.status(201).send('길드 정보 추가 성공');
-
-        } else {
-            const currentDate = new Date();
-            currentDate.setHours(0, 0, 0, 0);
-            currentDate.setDate(currentDate.getDate() - 1);
-            currentDate.setHours(currentDate.getHours() + 9);
-
-            if (guildExists.last_updated < currentDate) {
-                const guildMembers = await GuildService.updateGuild(guildName, worldName);
-                for (const guildMember of guildMembers) {
-                    const guildMemberExists = await CharacterService.getCharacter(guildMember);
-                    // console.log("guildMEmberExists", guildMemberExists);
-                    if (!guildMemberExists) {
-                        await CharacterService.createCharacter(guildName, worldName, guildMember);
-                    } else {
-                        await CharacterService.updateCharacter(guildName, worldName, guildMember);
-                    }
-                };
-                res.status(200).send('길드 정보 업데이트 성공');
-            } else {
-                res.status(200).send('이미 최신 정보입니다.');
-            }
-        }
+        await GuildPageService.createOrUpdateGuildPage(guildName, worldName);
+        res.status(200).send('데이터 업데이트 성공');
     } catch (error) {
-        console.error('에러 발생: ', error);
+        console.error('에러 발생:', error);
         res.status(500).send('서버 에러');
     }
 }
