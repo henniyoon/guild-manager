2ff7c1782beaa11e2cf87ff20320b075d7e01c4d
henniyoon <2749126@naver.com>
2024-02-18 04:36:39 +0900

    Header / Footer

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,15 +5,24 @@ import { useAuth } from './AuthContext';
 
 const Header: React.FC = () => {
   const { isLoggedIn, userInfo, logout } = useAuth();
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'inherit'
+  };
 
   return (
-    <header className="header">
-      <div className="logo">LOGO</div>
-      <h1 className="title">
-        <Link to="/">Guild Page</Link>
-      </h1>
+    <header className="header p-1" style={{ textAlign: 'center', padding: '10px 0' }}>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <Link to="/" style={linkStyle}>
+              <img src="/logo.png" style={{ maxHeight: '50px', marginRight: '10px' }} alt="Guild Manager Logo" />
+              길드매니저
+            </Link>
+          </a>
+        </div>
+      </nav>
       <nav className="nav">
-        <Link to="/">Home</Link>
         {!isLoggedIn && (
           <>
             <Link to="/SignUp">회원가입</Link>
@@ -22,7 +31,8 @@ const Header: React.FC = () => {
         )}
         {isLoggedIn && (
           <>
-            <span>{userInfo?.username}</span>
+            <Link to="/MyPage">{userInfo?.username}</Link>
+            {/* <span>{userInfo?.username}</span> */}
             <button onClick={logout}>로그아웃</button>
           </>
         )}
