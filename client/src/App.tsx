aa1edaf42e4e393ba314910b47314a3db7d03840
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2024-02-18 00:42:14 +0900

    필터 페이지 초안

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Graphpage from './pages/GraphPage/Graphpage';
 import Adminpage from './pages/AdminPage/Adminpage'
 import Loginpage from './pages/LoginPage/Loginpage';
 import SignUppage from './pages/SingUpPage/SignUppage';
+import Filterpage from './pages/FilterPage/Filterpage'
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
       <Route path="/Adminpage" element={<Adminpage />} />
       <Route path="/Login" element={<Loginpage />} />
       <Route path="/SignUp" element={<SignUppage />} />
+      <Route path="/Filter" element={<Filterpage />} />
     </Routes>
 
     </div>
