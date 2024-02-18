95cd34570652763e65b52449a6f0df2e22244b6b
henniyoon <2749126@naver.com>
2024-02-19 08:27:57 +0900

    페이지 레이아웃 설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,42 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
 import App from './App';
 import Header from './components/Header';
-import { AuthProvider } from './components/AuthContext';
+import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
+import { AuthProvider } from './components/AuthContext';
+import './styles/index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+// MUI의 기본 테마 생성
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#282a35',
+      main: '#282a35',
+      dark: '#282a35',
+      contrastText: '#fff',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
-        <Header />
-        <App />
-        <Footer />
+        <ThemeProvider theme={theme}>
+          {/* <Header /> */}
+          <App />
+          {/* <Footer /> */}
+        </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>
   </React.StrictMode>
