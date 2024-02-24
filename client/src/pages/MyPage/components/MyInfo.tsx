f93abe8e17ddd7152e2aadc98c9f439add98b5fc
henniyoon <2749126@naver.com>
2024-02-25 06:56:41 +0900

    마이페이지에서 서버로부터 userInfo 받아와서 출력하도록 제작

--- client/src/pages/MyPage/components/MyInfo.tsx
+++ client/src/pages/MyPage/components/MyInfo.tsx
@@ -1,18 +1,48 @@
-import React, { useState } from "react";
-import { Button, Input } from "@mui/material";
-// import { useAuth } from "../../../components/AuthContext";
+import React, { useEffect, useState } from "react";
+import { Button, Box, TextField, Typography } from "@mui/material";
+
+interface MyInfo {
+    username: string;
+    email: string;
+    guildName: string;
+    worldName: string;
+    role: string;
+}
+
+const token = localStorage.getItem("token");
 
 const MyInfo: React.FC = () => {
-    // const { userInfo } = useAuth();
+    const [userInfo, setUserInfo] = useState<MyInfo | null>(null);
     const [apiKey, setApiKey] = useState('');
     const [worldName, setWorldName] = useState('');
     const [guildName, setGuildName] = useState('');
 
-    const handleButtonClick = async () => {
-        const url = `/role`;
-        const token = localStorage.getItem("token");
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            try {
+                const response = await fetch("/myInfo", {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "application/json",
+                    },
+                });
 
-        fetch(url, {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setUserInfo(data);
+            } catch (error) {
+                console.error("데이터를 불러오는 데 실패했습니다:", error);
+            }
+        };
+        fetchUserInfo();
+    }, []); // 빈 의존성 배열을 사용하여 한 번만 호출되도록 설정
+
+    const handleButtonClick = async () => {
+        fetch("/role", {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -25,6 +55,7 @@ const MyInfo: React.FC = () => {
                 // 서버에서 반환한 데이터 처리
                 if (data === "길드 관리자 인증 성공") {
                     alert("성공적으로 인증되었습니다.");
+                    window.location.reload();
                 } else {
                     alert("인증에 실패하였습니다.");
                 }
@@ -35,20 +66,43 @@ const MyInfo: React.FC = () => {
     };
 
     return (
-        <div>
-            <h1>마이 페이지</h1>
+        <Box>
+            <Typography variant="h4" style={{ marginBottom: '20px', fontWeight: 'bold' }}>마이페이지</Typography>
+            
+            <Box style={{ marginBottom: '20px' }}>
+                <Typography variant="body1" style={{ marginBottom: '5px' }}>닉네임 : {userInfo?.username}</Typography>
+                <Typography variant="body1" style={{ marginBottom: '5px' }}>아이디 : {userInfo?.email}</Typography>
+                <Typography variant="body1" style={{ marginBottom: '5px' }}>길드명 : {userInfo?.guildName}</Typography>
+                <Typography variant="body1" style={{ marginBottom: '5px' }}>월드명 : {userInfo?.worldName}</Typography>
+                <Typography variant="body1" style={{ marginBottom: '5px' }}>권한 : {userInfo?.role}</Typography>
+            </Box>
 
-            <p>닉네임</p>
-            <p>아이디</p>
-            <Button>비밀번호 재설정</Button>
-            <p>API KEY</p>
-            <Input value={apiKey} onChange={(e) => setApiKey(e.target.value)}></Input>
-            <p>길드명</p>
-            <Input value={guildName} onChange={(e) => setGuildName(e.target.value)}></Input>
-            <p>월드명</p>
-            <Input value={worldName} onChange={(e) => setWorldName(e.target.value)}></Input>
-            <Button onClick={handleButtonClick}>길드 관리자 인증</Button>
-        </div>
+            <Box>
+                <form>
+                    <Typography variant="h5" style={{ marginBottom: '20px', fontWeight: 'bold' }}>길드 관리자 인증</Typography>
+                    <TextField
+                        label="API KEY"
+                        variant="outlined"
+                        value={apiKey}
+                        onChange={(e) => setApiKey(e.target.value)}
+                    />
+                    <TextField
+                        label="길드명"
+                        variant="outlined"
+                        value={guildName}
+                        onChange={(e) => setGuildName(e.target.value)}
+                    />
+                    <TextField
+                        label="월드명"
+                        variant="outlined"
+                        value={worldName}
+                        onChange={(e) => setWorldName(e.target.value)}
+                    />
+                    <Button variant="contained" type="button" onClick={handleButtonClick}>권한 받기</Button>
+                </form>
+            </Box>
+
+        </Box>
     );
 };
 
