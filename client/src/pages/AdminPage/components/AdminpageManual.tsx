74efa625756575aa55917230e562a60114ca1a5c
ChoiEunCheol <wndqhrgkdlel@gmail.com>
2024-02-25 03:09:39 +0900

    관리자 페이지 모달 임시

---
+++ client/src/pages/AdminPage/components/AdminpageManual.tsx
@@ -0,0 +1,11 @@
+// HomePageInstructions.tsx
+import React, { FC } from 'react';
+
+const AdminpageManual: FC = () => (
+  <div>
+    <h2>홈페이지 이용 방법</h2>
+    <p>여기에 홈페이지 이용 방법을 설명합니다...</p>
+  </div>
+);
+
+export default AdminpageManual;
