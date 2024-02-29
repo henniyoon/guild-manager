33a3377858fdb6c518de96113d7eb5e8f84a082f
최은철 <wndqhrgkdlel@gmail.com>
2024-02-29 15:13:52 +0900

    전체적인 css 수정 / mui 제거

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useAuth } from './AuthContext';
-import AppBar from '@mui/material/AppBar';
-import { Toolbar, Button, Typography } from '@mui/material';
-import styles from '../styles/Header.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import AppBar from "@mui/material/AppBar";
+import { Toolbar, Button, Typography } from "@mui/material";
+import styles from "../styles/Header.module.css";
 
 const Header: React.FC = () => {
   const { isLoggedIn, userInfo, logout } = useAuth();
   const handleLogout = () => {
     // 로그아웃 시 토큰을 제거하는 로직 추가
     // 예를 들어, localStorage에서 토큰을 삭제하는 경우
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     logout(); // 다른 로그아웃 로직 실행 (예: 세션 초기화 등)
   };
-  
+
   const renderAuthLinks = () => {
     if (!isLoggedIn) {
       return (
         <>
-          <Button component={Link} to="/SignUp" color="inherit" className={styles.link}>
+          <Button
+            component={Link}
+            to="/SignUp"
+            color="inherit"
+            className={styles.link}
+          >
             회원가입
           </Button>
-          <Button component={Link} to="/Login" color="inherit" className={styles.link}>
+          <Button
+            component={Link}
+            to="/Login"
+            color="inherit"
+            className={styles.link}
+          >
             로그인
           </Button>
         </>
@@ -29,10 +39,19 @@ const Header: React.FC = () => {
     } else {
       return (
         <>
-          <Button component={Link} to="/MyPage" color="inherit" className={styles.link}>
+          <Button
+            component={Link}
+            to="/MyPage"
+            color="inherit"
+            className={styles.link}
+          >
             {userInfo?.username}
           </Button>
-          <Button color="inherit" onClick={handleLogout} className={styles.link}>
+          <Button
+            color="inherit"
+            onClick={handleLogout}
+            className={styles.link}
+          >
             로그아웃
           </Button>
         </>
@@ -40,19 +59,16 @@ const Header: React.FC = () => {
     }
   };
 
-  const drawerWidth = 240;
-
   return (
-    <AppBar 
-    position="fixed"
-    sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-    >
-      <Toolbar>
-        
-        <div className={styles.grow} />
-        <div className={styles.authLinks}>{renderAuthLinks()}</div>
-      </Toolbar>
-    </AppBar>
+    <div className={styles.container}>
+      <img src="/logo.png" className={styles.logo} alt="Guild Manager Logo" />
+      <div style={{ marginLeft: "5px" }}>
+        <div className={styles.boldText}>메소</div>
+        <div className={styles.boldText}>메이플길드관리소</div>
+      </div>
+      <div className={styles.grow} />
+      <div className={styles.authLinks}>{renderAuthLinks()}</div>
+    </div>
   );
 };
 
