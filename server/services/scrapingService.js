921aeeb127f0a049d28d562eb83ea66c90dabce2
henniyoon <2749126@naver.com>
2024-03-09 23:42:55 +0900

    fetchGuildUrl 에서 스크래핑 요청 헤더에 user agent 추가 ip 밴 방지

--- server/services/scrapingService.js
+++ server/services/scrapingService.js
@@ -1,28 +1,31 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const worldNameToId = {
-    '리부트2': 1,
-    '리부트': 2,
-    '오로라': 3,
-    '레드': 4,
-    '이노시스': 5,
-    '유니온': 6,
-    '스카니아': 7,
-    '루나': 8,
-    '제니스': 9,
-    '크로아': 10,
-    '베라': 11,
-    '엘리시움': 12,
-    '아케인': 13,
-    '노바': 14
-};
-
 async function fetchGuildUrl(worldName, guildName) {
     try {
+        const worldNameToId = {
+            '리부트2': 1,
+            '리부트': 2,
+            '오로라': 3,
+            '레드': 4,
+            '이노시스': 5,
+            '유니온': 6,
+            '스카니아': 7,
+            '루나': 8,
+            '제니스': 9,
+            '크로아': 10,
+            '베라': 11,
+            '엘리시움': 12,
+            '아케인': 13,
+            '노바': 14
+        };
         const worldId = worldNameToId[worldName] || 0;
         const gidUrl = `https://maplestory.nexon.com/N23Ranking/World/Guild?w=${worldId}&t=1&n=${guildName}`;
-        const gidResponse = await axios.get(gidUrl);
+        const gidResponse = await axios.get(gidUrl, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            },
+        });
         const $ = cheerio.load(gidResponse.data);
         const link = $('td.left a').attr('href');
         return `https://maplestory.nexon.com/${link}&orderby=1`;
@@ -47,5 +50,6 @@ async function scrapeSubMaster(worldName, guildName) {
 }
 
 module.exports = {
+    fetchGuildUrl,
     scrapeSubMaster,
 }
