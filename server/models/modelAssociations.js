0f142393744872528186459a32c6322b22f86b97
henniyoon <2749126@naver.com>
2024-03-10 01:05:35 +0900

    외래키 관계 추가

--- server/models/modelAssociations.js
+++ server/models/modelAssociations.js
@@ -1,9 +1,13 @@
 const User = require('./User.js');
 const Guild = require('./Guild.js');
 const World = require('./World.js');
+const Character = require('./Character.js');
 
 User.belongsTo(Guild, { foreignKey: 'guild_id', as: 'guild' });
 Guild.hasMany(User, { foreignKey: 'guild_id', as: 'members' });
 
 Guild.belongsTo(World, { foreignKey: 'world_id', as: 'world' });
 World.hasMany(Guild, { foreignKey: 'world_id' });
+
+Character.belongsTo(Guild, { foreignKey: 'guild_id', as: 'guild' });
+Guild.hasMany(Character, { foreignKey: 'guild_id', as: 'characters' });
